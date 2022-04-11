9500640113bb8089946859b037704953c90830ab
ivansimplicio <ivansimplicio@outlook.com>
2022-04-11 08:33:37 -0300

    feat: function connection to RDS database

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -4,10 +4,29 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
+import mysql from 'mysql';
+
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+
+  const connection = mysql.createConnection({
+    host: 'serverless-api.cwxfbjetigjo.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'senha123',
+    database: 'app'
+  });
+
+  const p = new Promise((resolve) => {
+    connection.query('SELECT * FROM users', function (err, results){
+      resolve(results);
+    });
+  });
+
+  const result = await p;
+
+  const json = JSON.stringify(result)
+
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
+    message: `Hello ${event.body.name}, ${json}`,
   });
 };
 
--- src/functions/hello/index.ts
+++ src/functions/hello/index.ts
@@ -1,5 +1,6 @@
-import schema from './schema';
 import { handlerPath } from '@libs/handler-resolver';
+
+import schema from './schema';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
@@ -16,4 +17,17 @@ export default {
       },
     },
   ],
+  vpc: {
+    securityGroupIds: [
+      "sg-0c0a38f5029d52944"
+    ],
+    subnetIds: [
+      "subnet-05eab8a66473d8dd7",
+      "subnet-08fdc8f46ee89733d",
+      "subnet-07fa0eb93053cfc9d",
+      "subnet-036a1a12df6a9e8b4",
+      "subnet-09c05fa1be8f9891c",
+      "subnet-0dbad941deb5c9eb4"
+    ]
+  }
 };
